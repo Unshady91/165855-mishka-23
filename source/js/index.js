04ae04dafb61b477ca8930137bcd6523b2ccc1bd
Azamat M <amoldashev@gmail.com>
2021-09-02 07:35:15 +0500

    Добавляет кнопки в навигацию

---
+++ source/js/index.js
@@ -0,0 +1,19 @@
+'use strict'
+const pageNav = document.querySelector('.page-nav');
+const pageNavOpen = document.querySelector('.page-nav--open');
+const pageNavButton = document.querySelector('.page-nav__button');
+
+pageNav.classList.remove('page-nav--nojs');
+
+pageNavButton.addEventListener(
+  'click',
+  (evt) => {
+    if (pageNav.classList.contains('page-nav--closed')) {
+      pageNav.classList.remove('page-nav--closed');
+      pageNav.classList.add('page-nav--open');
+    } else {
+      pageNav.classList.add('page-nav--closed');
+      pageNav.classList.remove('page-nav--open');
+    }
+  }
+)
